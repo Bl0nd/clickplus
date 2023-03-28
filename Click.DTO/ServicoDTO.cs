1d9df9340e18f1cc46dd599a79da2a36da7634ca
FERNANDA ARAUJO DIAS ESPIRITO SANTO <fernanda.adesanto@senacsp.edu.br>
2023-03-28 17:28:04 -0300

    DTO

---
+++ Click.DTO/ServicoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Click.DTO
+{
+    public class ServicoDTO
+    {
+        public int idServico { get; set; }
+        public string produto { get; set; }
+        public string url { get; set; }
+    }
+}
